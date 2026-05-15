9999e38fcc75ebeded0448020744d83d1f296b73
Arthas3000 <lbguibes2010@gmail.com>
2026-05-14 22:20:52 -0300

    Delete PORTFOLIO - ENG directory

--- PORTFOLIO - ENG/js/main.js
+++
@@ -1,9 +0,0 @@
-import { initNavbar } from './ui-components.js';
-import { initRevealAnimations } from './animations.js';
-import { initVideoScrollSync } from './video-sync.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-    initNavbar('navbar');
-    initRevealAnimations();
-    initVideoScrollSync('scroll-video');
-});
